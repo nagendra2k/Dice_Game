52e625e3550de514122ac4afea5fcebcf735e4d5
nagendra velagana <76590279+nagendra2k@users.noreply.github.com>
2021-09-27 17:47:58 +0530

    Add Dice Challenge Files

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+document.querySelector(".image1").setAttribute("src","images/dice"+randomNumber1+".png");
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+document.querySelector(".image2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+
+if(randomNumber1 > randomNumber2){
+document.querySelector("h1").innerHTML = "🚩player 1 have won the game";
+}
+else if(randomNumber1 < randomNumber2){
+document.querySelector("h1").innerHTML = "player 2 have won the game🚩";
+}
+else{
+  document.querySelector("h1").innerHTML = "The game is draw";
+}
